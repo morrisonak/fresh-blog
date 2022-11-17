008240ce1f275be7351b3122b17f7d6a7741a498
morrisonak <57382686+morrisonak@users.noreply.github.com>
2022-11-17 14:45:09 -0600

    Update site.ts

--- data/site.ts
+++ data/site.ts
@@ -3,7 +3,7 @@ export const site = {
   description: 'A blog built with Fresh',
   // This should be a url to the static file you're using for your open graph image
   ogImage: 'https://fresh-blog.littlesticks.dev/og-image.png',
-  copyrightName: 'Fresh Blog',
+  copyrightName: 'SpikeJab.com',
   // The url of the source code for the site, used in the footer.
   viewSourceUrl: 'https://github.com/littlesticks/fresh-blog'
 }
